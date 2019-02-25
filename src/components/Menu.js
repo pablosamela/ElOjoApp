bf176ecc257195e4dfb58134966ebe5139a5f16f
pablosamela <psamela@gmail.com>
2019-02-25 00:17:09 -0300

    Adding http request to server

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
-import sections from '../data/sections';
 
 class Menu extends Component {
   
-  constructor (){
-    super();
+  constructor (props){
+    super(props);
     this.state = {
+      error: null,
+      isLoaded: false,
       sections: []
     }
     this.loadFromServer = this.loadFromServer.bind(this);
  
   }
   
-  
   loadFromServer () {
-    fetch('http://www.elojodigital.com/all-sections-json').then(
-      (response) => {
-        this.setState({ sections: response.json() });
+    fetch(
+      'http://www.elojodigital.com/all-sections-json',
+      {
+        mode: "cors",
+        method: 'GET',
+        headers: {'Content-Type':'application/json'}
       }
-    ).bind(this);
+    )
+    .then(
+      res => res.json()
+    )
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          sections: result.sections
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
   }
   
   componentDidMount() {
@@ -26,33 +46,37 @@ class Menu extends Component {
   }
 
   render() {
-    const sections = this.state.sections.map((section,i) => {
+    const { error, isLoaded, sections } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
       return (
-        <li className="nav-item active">
-          <a href="{sections.id}" className="nav-link" title="{ sections.description }">{ sections.name }</a>
-        </li>
-      )
-    });
+        <nav className="navigation navbar navbar-black bg-black navbar-expand-lg">
+          <div className="container">
+            <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-    return (
-      <nav className="navigation navbar navbar-black bg-black navbar-expand-lg">
-        <div className="container">
-          <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="navbar-collapse mx-auto collapse" id="mainNavbar">
-            <ul className="navbar-nav text-uppercase">
-            <li className="nav-item active">
-              <a href="/" className="nav-link" title="El Ojo digital">Inicio</a>
-            </li>
-            { sections }
-            
-            </ul>
+            <div className="navbar-collapse mx-auto collapse" id="mainNavbar">
+              <ul className="navbar-nav text-uppercase">
+              <li className="nav-item active">
+                <a href="/" className="nav-link" title="El Ojo digital">Inicio</a>
+              </li>
+                { sections.map((section,i) => (
+                  <li className="nav-item active">
+                    <a href="{sections.id}" className="nav-link" title="{ sections.description }">
+                    { sections.name }
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    );
+        </nav>
+      );
+    }
   }
 }
 
